f74cecde6c9f92ebdf455de387e855fac0ea004c
Rick <rleander@comcast.net>
2022-11-28 11:16:17 -0700

    one file not saved

--- src/statements.py
+++ src/statements.py
@@ -1,42 +1,5 @@
 #  statements - used as both command and runtime
 
-#  note: line numvers will be removed before passing to these functions
-
 import data
 import helpers
 
-# let statement
-
-def stmtLet(cmdWork):
-  parts = cmdWork.split()
-  if (len(parts) < 4):
-    return 'Syntax error'
-	
-  if (parts[2] != '='):
-    return 'Syntax error'
-      
-  vName = parts[1]
-  i = cmdWork.find('=')
-  expr = cmdWork[i+2: len(cmdWork)] 
-  try:
-    value = eval(expr, data.variables)
-  except:
-    return 'Syntax error'
-  data.variables[vName] = value  
-  return 'OK'
-  
-#print statement
-  
-def stmtPrint(cmdWork):
-  parts = cmdWork.split()
-  if (len(parts) < 2):
-    return 'Syntax error'
-  
-  i = cmdWork.find(' ')
-  expr = cmdWork[i+1: len(cmdWork)]
-  try:
-    value = eval(expr, data.variables)
-  except:
-    return  'Syntax error'
-  return str(value)
-  
