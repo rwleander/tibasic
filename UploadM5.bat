151efffaefaca84d8a41082c10ee1c39c8bbabd3
Rick <rleander@comcast.net>
2022-12-09 08:50:45 -0700

    added upload script for M5 Stick

---
+++ UploadM5.bat
@@ -0,0 +1,7 @@
+ampy -p com3 put src/helpers.py
+ampy -p com3 put src/data.py
+ampy -p com3 put src/commands.py
+ampy -p com3 put src/parser.py
+ampy -p com3 put src/runtime.py
+ampy -p com3 put src/main.py
+ampy -p com3 ls
